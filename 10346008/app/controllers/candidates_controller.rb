c93feff39158b77776f66c59aeeb3db3c3f9a579
iamloveez <iamloveez@gmail.com>
2017-05-04 23:08:39 -0400

    finish edit book feature

--- 10346008/app/controllers/candidates_controller.rb
+++ 10346008/app/controllers/candidates_controller.rb
@@ -1,10 +1,13 @@
 class CandidatesController < ApplicationController
+  
     def index
       @candidates = Candidate.all
     end
+    #新增書籍
     def new
       @candidate = Candidate.new
     end
+  
     def create
       @candidate = Candidate.new(candidate_params)
 
@@ -16,6 +19,22 @@ class CandidatesController < ApplicationController
         render :new
       end
     end
+    #編輯書籍
+    def edit
+      @candidate = Candidate.find_by(id: params[:id])
+    end
+    def update
+      @candidate = Candidate.find_by(id: params[:id])
+
+      if @candidate.update(candidate_params)
+        # 成功
+        redirect_to candidates_path, notice: "資料更新成功!"
+      else
+        # 失敗
+        render :edit
+      end
+    end
+  
     private
     def candidate_params
       params.require(:candidate).permit(:bookname, :introduction, :name, :price)
